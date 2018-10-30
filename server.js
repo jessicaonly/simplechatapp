b60cdd921cc737fb951c7b49ba17e1b7a5f7c88c
Jessica Skok <jessica.skok@gmail.com>
2018-10-29 19:10:51 -0700

    add in functinality to post messages

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ io.sockets.on('connection', function(socket){
   console.log('Connected: %s sockets connected', connections.length);
 
   //disconnect
+  socket.on('disconnect', function(){
   connections.splice(connections.indexOf(socket), 1);
   console.log('Disconnected: %s sockets connected', connections.length);
+  });
+
+  //Send Message
+  socket.on('send message', function(data){
+    io.sockets.emit('new message', {msg: data});
+  })
 });
