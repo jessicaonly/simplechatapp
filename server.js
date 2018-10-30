852e4b272246d88eb8819f65e850d8ebfa0eb521
Jessica Skok <jessica.skok@gmail.com>
2018-10-29 19:22:28 -0700

    add username functionality to sockets on back end

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ io.sockets.on('connection', function(socket){
 
   //disconnect
   socket.on('disconnect', function(){
+    if (!socket.username) return;
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
   connections.splice(connections.indexOf(socket), 1);
   console.log('Disconnected: %s sockets connected', connections.length);
   });
@@ -26,4 +29,16 @@ io.sockets.on('connection', function(socket){
   socket.on('send message', function(data){
     io.sockets.emit('new message', {msg: data});
   })
+
+  //New User
+  socket.on('new user', function(data, callback){
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+  function updateUsernames(){
+    io.sockets.emit('get users', usernames);
+  }
 });
